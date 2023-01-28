95d0191a319cb7d7c97290a4b42829d8f234be9c
Talhary <talhariaz5425869@gmail.com>
2023-01-28 18:20:21 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -14,28 +14,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['923101502365'] //ur owner number
+global.ownername = "Talha riaz" //ur owner name
+global.ytname = "YT: no channel " //ur yt chanel name
+global.socialm = "GitHub: Talhary" //ur github or insta name
+global.location = "Islamabad, pakistan" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
-global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.ownernomer = "923101502365" //ur number
+global.premium = ['923101502365'] //ur premium number
+global.botname = 'RED_DEVIL' //ur bot name
+global.linkz = "no link " //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = '' //script link
+global.themeemoji = "('-')" //ur theme emoji
+global.packname = "TALHA STICKERS" //ur sticker watermark packname
+global.author = "talha_riaz" //ur sticker watermark author
+global.wm = "don't care issues" //ur watermark
 
 // Other
 global.sessionName = 'session'
-global.prefa = ['','!','.','#','&']
-global.sp = ''
+global.prefa = [' ','!','.','#','&']
+global.sp = ' '
 
 //messages
 global.mess = {
